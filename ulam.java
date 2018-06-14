a4c3df7b7a9da9a0cdcd13edc8c279de73026abf
MoldingTofu <jeremykchang@gmail.com>
2018-06-13 23:07:52 -0500

    prints prime spirals with " " if number is not prime

--- ulam.java
+++ ulam.java
@@ -1,43 +1,59 @@
 public class ulam {
   public static void main(String[] args) {
-    int[][] row = spiral(5);
+    int[][] row = spiral(11);
 
     for(int i = 0; i < row.length; i++) {
       for(int j = 0; j < row.length; j++) {
-        System.out.print(row[i][j] + "\t");
+        if(isPrime(row[i][j])) {
+          System.out.print(row[i][j] + "\t");
+        } else {
+          System.out.print(" " + "\t");
+        }
       }
       System.out.print("\n");
+    }
+  }
+  public static boolean isPrime(int num) {
+    if(num == 1) {
+      return false;
+    } else if (num == 2) {
+      return true;
+    } else {
+      for(int i = num - 1; i >= Math.sqrt(num); i--) {
+        if(num % i == 0) {
+          return false;
+        }
+      }
+      return true;
     }
   }
 
   public static int[][] spiral(int input) {
     int[][] row = new int[input][input];
-    int i = 1;
+    int i = input * input;
     int min = 0;
     int max = input - 1;
 
-    while(true){
-      for(int j = min; j <= max; j++) {
-        row[min][j] = i;
-        i++;
-      }
-      for(int j = min + 1; j <= max; j++)   {
-        row[j][max] = i;
-        i++;
+    while(i > 0){
+      for(int j = max; j >= min; j--) {
+        row[max][j] = i;
+        i--;
       }
       for(int j = max - 1; j >= min; j--) {
-        row[max][j] = i;
-        i++;
-      }
-      for(int j = max - 1; j > min; j--){
         row[j][min] = i;
-        i++;
+        i--;
       }
+      for(int j = min + 1; j <= max; j++) {
+        row[min][j] = i;
+        i--;
+      }
+      for(int j = min + 1; j < max; j++) {
+        row[j][max] = i;
+        i--;
+      }
+
       min++;
       max--;
-      if(i > input * input) {
-        break;
-      }
     }
 
     return row;
